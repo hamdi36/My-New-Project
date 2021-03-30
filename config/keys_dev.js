9265af72a3b5ef0b920e8f1cff37f0edcc4458c2
hamdi <hamdibenjemaa36@gmail.com>
2021-03-30 14:19:16 +0100

    project

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    //mongoURI: "mongodb+srv://wassimkhleyfi007:06166595@cluster0.jdl1q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+//    mongoURI: "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false",
+  mongoURI :"mongodb+srv://Hamdi36:hamdi36@cluster0.qiyao.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+secretKey: "06166595"
+}
